ec533429ceaebf011567a1b71619a5b0e4f2ccff
LordChonk <adrian.hjert@gmail.com>
2025-09-18 02:11:32 +0200

    solved file version error

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Navbar(){
     return (
         <nav>
-            <a href="#">Start</a> | <a href="#">Om oss</a> | <a href="#">Kontat</a>
+            <a href="#">Start</a> | <a href="#">Om oss</a> | <a href="#">Kontakt</a>
         </nav>
     );
 }
