71510af87ee271a7de3a56e9e42405573eb8b372
LordChonk <adrian.hjert@gmail.com>
2025-11-04 23:38:55 +0100

    page p much done, just small adjustments to be made

--- src/pages/OmOss.tsx
+++ src/pages/OmOss.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function OmOss(){
     return (
-        <div>
+        <div className="om-oss">
             <h2>Om Oss</h2>
             <p>Med mångårig erfarenhet inom bilhandel, ett brett utbud av kvalitetsbilar och ett starkt fokus på kundnöjdhet, är vårt motto att hitta den optimala bilen för varje kund.
 VK Performance grundades ur en passion för premiumbilar – fordon som kombinerar kraftfull prestanda med elegant design. Vi erbjuder bilar som inte bara lever upp till höga krav, utan som även levererar en körupplevelse utöver det vanliga.</p>
--- src/pages/Startsida.tsx
+++ src/pages/Startsida.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Startsida(){
     return (
-        <div>
+        <div className="start">
             <h2>Välkommen till oss på VK Performance</h2>
             <p>Din bilhandlare med fokus på kvalitet och kundnöjdhet
 VK Performance är din pålitliga bilhandlare i Knivsta – med ett brett utbud av bilar för alla behov och budgetar. Med vår erfarenhet och personliga service hjälper vi dig att hitta den optimala bilen som passar just dig.</p>
